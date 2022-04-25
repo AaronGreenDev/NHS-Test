60ed5f4da6b920d83029c36eafdb922e2781b2cf
AaronGreenDev <aarongreen@outlook.com>
2022-04-25 17:23:24 +0100

    added nhs logo

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const app = document.getElementById('root')
 
-//const logo = document.createElement('img')
-//logo.src = 'https://www.recoveryandwellbeing.co.uk/images/NHSLogo.png'
+
 
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
